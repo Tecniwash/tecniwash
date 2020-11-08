918bff436fb96f8c820e741e51959849907909e5
Juan Izaguirre <juanfilpz@gmail.com>
2020-11-07 22:40:03 -0600

    conexion actualizacion

--- config/conexion2.php
+++ config/conexion2.php
@@ -1,6 +1,6 @@
 <?php
 
-  $mysqli = new mysqli("testingdb.c2lgpurhnyvd.us-east-1.rds.amazonaws.com", "root", "NiOlUT2X3M1D2maxbCWI", "bd_tw");
+  $conn = new mysqli("testingdb.c2lgpurhnyvd.us-east-1.rds.amazonaws.com", "root", "NiOlUT2X3M1D2maxbCWI", "bd_tw");
 if (mysqli_connect_errno()) {
 	echo 'Conexion Fallida : ', mysqli_connect_error();
 	exit();
